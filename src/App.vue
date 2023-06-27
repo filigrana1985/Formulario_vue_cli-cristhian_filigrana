e014822456cadfa912dc50334275ba558a4bf134
Cristhian Filigrana <adesarrollo1@unico.com.co>
2023-06-27 14:48:40 -0500

    Correcion, se separa componente formulario

--- src/App.vue
+++ src/App.vue
@@ -1,117 +1,27 @@
 <template>
-  <div class="container">
-    <h2 class="h2 text-center text-danger">Formulario de Datos</h2>
-    <form @submit.prevent="submitForm" ref="formulario" class="form-container">
-      <div class="form-group">
-        <label>Nombre:</label>
-        <input type="text" class="form-control" v-model="nombre" required>
-      </div>
-
-      <div class="form-group">
-        <label>Email:</label>
-        <input type="email" class="form-control" v-model="email" required>
-      </div>
-
-      <div class="form-group">
-        <label>Edad:</label>
-        <input type="number" class="form-control" v-model="edad" min="0" required>
-      </div>
-
-      <div class="form-group">
-        <label>Teléfono:</label>
-        <input type="tel" class="form-control" v-model="telefono" pattern="[0-9]{10}" required>
-      </div>
-
-      <button type="submit" class="btn btn-success">Enviar</button>
-    </form>
-
-    <hr>
-
-    <h2>Datos ingresados:</h2>
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th>Nombre</th>
-          <th>Email</th>
-          <th>Edad</th>
-          <th>Teléfono</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(persona, index) in personas" :key="index">
-          <td>{{ persona.nombre }}</td>
-          <td>{{ persona.email }}</td>
-          <td>{{ persona.edad }}</td>
-          <td>{{ persona.telefono }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div>
+    <Formulario @form-submit="handleFormSubmit" />
+    <Tabla :personas="personas" />
   </div>
 </template>
 
-<style>
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-}
-
-.h2 {
-  text-align: center;
-  color: red;
-}
-
-.form-container {
-  max-width: 400px;
-  margin: 0 auto;
-}
-
-.form-group {
-  margin-bottom: 10px;
-}
-
-label {
-  display: block;
-}
-
-input[type="text"],
-input[type="email"],
-input[type="number"],
-input[type="tel"] {
-  width: 100%;
-}
-</style>
-
 <script>
+import Formulario from './components/Formulario.vue';
+import Tabla from './components/Tabla.vue';
+
 export default {
+  components: {
+    Formulario,
+    Tabla
+  },
   data() {
     return {
-      nombre: '',
-      email: '',
-      edad: '',
-      telefono: '',
       personas: []
     };
   },
   methods: {
-    submitForm() {
-      if (this.$refs.formulario.checkValidity()) {
-        this.personas.push({
-          nombre: this.nombre,
-          email: this.email,
-          edad: this.edad,
-          telefono: this.telefono
-        });
-        this.resetForm();
-      }
-    },
-    resetForm() {
-      this.nombre = '';
-      this.email = '';
-      this.edad = '';
-      this.telefono = '';
+    handleFormSubmit(persona) {
+      this.personas.push(persona);
     }
   }
 };
---
+++ src/components/Formulario.vue
@@ -0,0 +1,80 @@
+<template>
+    <div  class="form-group mx-auto">
+      <h2>Formulario de Datos</h2>
+      <form @submit.prevent="submitForm" ref="formulario">
+        <label>Nombre:</label>
+        <input type="text" v-model="nombre" required class="form-input">
+        <br>
+  
+        <label>Email:</label>
+        <input type="email" v-model="email" required class="form-input">
+        <br>
+  
+        <label>Edad:</label>
+        <input type="number" v-model="edad" min="0" required class="form-input">
+        <br>
+  
+        <label>Teléfono:</label>
+        <input type="tel" v-model="telefono" pattern="[0-9]{10}" required class="form-input">
+        <br>
+  
+        <button type="submit" class="form-button">Enviar</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'FormularioVue',
+    data() {
+      return {
+        nombre: '',
+        email: '',
+        edad: '',
+        telefono: ''
+      };
+    },
+    methods: {
+      submitForm() {
+        if (this.$refs.formulario.checkValidity()) {
+          this.$emit('form-submit', {
+            nombre: this.nombre,
+            email: this.email,
+            edad: this.edad,
+            telefono: this.telefono
+          });
+          this.resetForm();
+        }
+      },
+      resetForm() {
+        this.nombre = '';
+        this.email = '';
+        this.edad = '';
+        this.telefono = '';
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  .form-input {
+    margin-bottom: 10px;
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+  
+  .form-button {
+    padding: 10px 20px;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+  .form-button:hover {
+    background-color: #45a049;
+  }
+  </style>
+  
